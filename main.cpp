a81bbf1345e6ff77280cef8459dd097370d7a484
davjd <davidbepic@gmail.com>
2019-12-17 01:23:46 -0500

    Cleaned main file.

--- main.cpp
+++ main.cpp
@@ -14,21 +14,22 @@
 #include "Document.h"
 #include "Node.h"
 
-// usage:
-
-// compare <type of season> <player_name> <player_name> .....
+// Usage:
+// -compare <type of season> <player_name> <player_name> .....
 // pull all stats for the given type of season for all given players.
 // could use cout color to depict which player is better.
 
-// get <player_name>
+// -pull <player_name>
 // basic command where all stats brought for given player.
 
-// fun_fact <empty>
-// lists a random fun fact from a list of fun facts.
+// -fun <empty>
+// Gets a random fun fact from a list of fun facts.
 // List: youngest/oldest to win a championship, youngest/oldest to win MVP,
 // players with more steals than fouls, players with more assists than points,
 // graph with most college going pro, GOAT college (most achievements), biggest
 // draft steals of all time.
+
+// Pull is the only command fully implemented.
 
 int main() {
   auto engine = std::make_unique<hoops::HoopsEngine>();
